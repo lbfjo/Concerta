949eee37f06def8399185580fc52dcb4b5b5c994
Francisco Bruno <franciscobruno@MBPdeFrancisco.lan>
2023-02-16 21:25:40 +0000

    changed layout

--- src/sections/feature.js
+++ src/sections/feature.js
@@ -1,41 +1,41 @@
-import React, { useRef } from 'react';
-import { Box, Container } from 'theme-ui';
-import BlockTitle from 'components/block-title';
-import Swiper from 'react-id-swiper';
+import React, { useRef } from "react";
+import { Box, Container } from "theme-ui";
+import BlockTitle from "components/block-title";
+import Swiper from "react-id-swiper";
 
-import FeatureCard from 'components/feature-card';
-import { FaLongArrowAltLeft, FaLongArrowAltRight } from 'react-icons/fa';
-import featureImage1 from 'assets/tutorial-1-1.png';
-import featureImage2 from 'assets/tutorial-1-2.png';
-import featureImage3 from 'assets/tutorial-1-3.png';
+import FeatureCard from "components/feature-card";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+import featureImage1 from "assets/tutorial-1-1.png";
+import featureImage2 from "assets/tutorial-1-2.png";
+import featureImage3 from "assets/tutorial-1-3.png";
 
 const FeatureData = [
   {
     image: featureImage1,
-    title: 'How to work with prototype design with adobe xd featuring tools',
-    comments: '22 Comments',
-    path: '/',
+    title: "How to work with prototype design with adobe xd featuring tools",
+    comments: "22 Comments",
+    path: "/",
   },
   {
     image: featureImage2,
-    title: 'Create multiple artboard by using figma prototyping development',
-    comments: '15 Comments',
-    path: '/',
+    title: "Create multiple artboard by using figma prototyping development",
+    comments: "15 Comments",
+    path: "/",
   },
   {
     image: featureImage3,
     title:
-      'Convert your web layout theming easily with sketch zeplin extension',
-    comments: '12 Comments',
-    path: '/',
+      "Convert your web layout theming easily with sketch zeplin extension",
+    comments: "12 Comments",
+    path: "/",
   },
-{
-  image: featureImage2,
-  title:
-    'Convert yo2ur web layout theming easily with sketch zeplin extension',
-  comments: '12 Comments',
-  path: '/',
-},
+  {
+    image: featureImage2,
+    title:
+      "Convert yo2ur web layout theming easily with sketch zeplin extension",
+    comments: "12 Comments",
+    path: "/",
+  },
 ];
 
 const Feature = () => {
@@ -91,11 +91,11 @@ const Feature = () => {
   return (
     <Box sx={styles.features} id="news">
       <Container>
-        <BlockTitle
+        {/* <BlockTitle
           slogan="Quality features"
           title="Tutorials that people love most"
           styles={styles.blockTitle}
-        />
+        /> */}
 
         <Swiper {...params} ref={ref}>
           {FeatureData.map((feature, index) => (
@@ -126,34 +126,34 @@ export default Feature;
 
 const styles = {
   blockTitle: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   features: {
-    pt: ['80px', null, null, null, null, null, '120px'],
-    pb: ['80px', null, null, null, '170px'],
-    backgroundColor: '#F8FAFC',
-    '.swiper-slider': {
-      overflowY: 'visible',
-      overflowX: 'hidden',
+    pt: ["80px", null, null, null, null, null, "120px"],
+    pb: ["80px", null, null, null, "170px"],
+    backgroundColor: "#F8FAFC",
+    ".swiper-slider": {
+      overflowY: "visible",
+      overflowX: "hidden",
     },
   },
   carouselBtns: {
-    display: ['flex', null, null, null, null, 'none'],
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: ["flex", null, null, null, null, "none"],
+    justifyContent: "center",
+    alignItems: "center",
     button: {
-      border: 'none',
-      outline: 'none',
-      backgroundColor: 'transparent',
+      border: "none",
+      outline: "none",
+      backgroundColor: "transparent",
       fontSize: [2, null, 4, null, 5],
-      color: '#BBC7D7',
-      width: 'auto',
+      color: "#BBC7D7",
+      width: "auto",
       padding: [0],
-      margin: '0 5px',
-      mt: '15px',
-      transition: '500ms',
-      '&:hover, &:active, &:focus': {
-        color: 'primary',
+      margin: "0 5px",
+      mt: "15px",
+      transition: "500ms",
+      "&:hover, &:active, &:focus": {
+        color: "primary",
       },
     },
   },
--- src/sections/services.js
+++ src/sections/services.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import { Box, Container, Grid, Heading, Text } from 'theme-ui';
-import BlockTitle from 'components/block-title';
-import Image from 'components/image';
+import React from "react";
+import { Box, Container, Grid, Heading, Text } from "theme-ui";
+import BlockTitle from "components/block-title";
+import Image from "components/image";
 
-import icon1 from 'assets/icons/BEMESTAR.png';
-import icon2 from 'assets/icons/Sustentabilidade.png';
-import icon3 from 'assets/icons/OneHealth.png';
+import icon1 from "assets/icons/BEMESTAR.png";
+import icon2 from "assets/icons/Sustentabilidade.png";
+import icon3 from "assets/icons/OneHealth.png";
 
 const SERVICES_DATA = [
   {
     icon: icon3,
-    title: 'One Health',
-    text:
-      'Reducing the distance between consumers and producers means also thinking about the concept of One Health, how the actions taken in one end can have great impact in the other. ',
+    title: "One Health",
+    text: "Reducing the distance between consumers and producers means also thinking about the concept of One Health, how the actions taken in one end can have great impact in the other. ",
   },
   {
     icon: icon2,
-    title: 'Sustainability',
-    text:
-      'Aligned with the concept of One Health , our approach to the Sustainability concept lies on the need to implement greener, cleaner production practices in many of nowadays production systems.',
+    title: "Sustainability",
+    text: "Aligned with the concept of One Health , our approach to the Sustainability concept lies on the need to implement greener, cleaner production practices in many of nowadays production systems.",
   },
   {
     icon: icon1,
-    title: 'Animal Welfare',
-    text:
-      'Animal Welfare is were it all started. The growing awareness of the consumers concerning animal welfare and production methodologies has led to an increase in the search of welfare certifications. ',
+    title: "Animal Welfare",
+    text: "Animal Welfare is were it all started. The growing awareness of the consumers concerning animal welfare and production methodologies has led to an increase in the search of welfare certifications. ",
   },
 ];
 
@@ -32,11 +29,11 @@ const Services = () => {
   return (
     <Box sx={styles.services} id="services">
       <Container>
-        <BlockTitle
+        {/* <BlockTitle
           slogan="Quality features"
           title="Concerta Principles"
           styles={styles.blockTitle}
-        />
+        /> */}
         <Grid sx={styles.grid}>
           {SERVICES_DATA.map((service, index) => (
             <Box
@@ -61,64 +58,53 @@ export default Services;
 
 const styles = {
   services: {
-    pt: ['80px', null, null, null, null, null, '140px'],
-    '.service-card:nth-of-type(2)': {
-      '.service-icon': {
-      },
+    pt: ["80px", null, null, null, null, null, "140px"],
+    ".service-card:nth-of-type(2)": {
+      ".service-icon": {},
     },
-    '.service-card:nth-of-type(3)': {
-      '.service-icon': {
-      },
+    ".service-card:nth-of-type(3)": {
+      ".service-icon": {},
     },
-    '.service-card:nth-of-type(4)': {
-      '.service-icon': {
-      },
+    ".service-card:nth-of-type(4)": {
+      ".service-icon": {},
     },
   },
   blockTitle: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   grid: {
-    display: 'grid',
-    gridGap: ['30px', null, null, null, null, '60px'],
-    gridTemplateColumns: [
-      '1fr',
-      null,
-      null,
-      '1fr 1fr',
-      null,
-      '1fr 1fr 1fr',
-    ],
+    display: "grid",
+    gridGap: ["30px", null, null, null, null, "60px"],
+    gridTemplateColumns: ["1fr", null, null, "1fr 1fr", null, "1fr 1fr 1fr"],
   },
   icon: {
-    display: 'flex',
-    ml: 'auto',
-    mr: 'auto',
-    width: ['80px', null, null, '110px'],
-    height: ['80px', null, null, '110px'],
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: ['20px', null, null, '40px'],
-   
+    display: "flex",
+    ml: "auto",
+    mr: "auto",
+    width: ["80px", null, null, "110px"],
+    height: ["80px", null, null, "110px"],
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: ["20px", null, null, "40px"],
   },
   serviceCard: {
-    textAlign: 'center',
+    textAlign: "center",
     h3: {
       margin: 0,
-      fontSize: ['18px', null, null, 3],
-      fontWeight: 'bold',
+      fontSize: ["18px", null, null, 3],
+      fontWeight: "bold",
       lineHeight: 1,
-      color: 'black',
-      mt: ['30px', null, null],
-      mb: ['20px', null, null],
+      color: "black",
+      mt: ["30px", null, null],
+      mb: ["20px", null, null],
     },
     p: {
       margin: 0,
-      fontSize: [0, null, null, '15px'],
-      color: 'heading_secondary',
-      width: '100%',
-      maxWidth: [null, null, null, null, '84%', '100%'],
-      mx: [null, null, null, null, 'auto', '0'],
+      fontSize: [0, null, null, "15px"],
+      color: "heading_secondary",
+      width: "100%",
+      maxWidth: [null, null, null, null, "84%", "100%"],
+      mx: [null, null, null, null, "auto", "0"],
     },
   },
 };
